8fd92c6f93a617ee9f0892a4151cf35d6d0e0e47
Afzaal Web <afzaalwebsolutions@gmail.com>
2025-06-18 14:53:33 +0500

    some more reg exp added

--- script.js
+++ script.js
@@ -30,22 +30,55 @@ arr.some(letter => letter === letter.toUpperCase());
 reference. You can define a capture group by wrapping a part of your expression in parentheses. For example, 
 /h(i|ey) camper/ would match either hi camper or hey camper, and would capture i or ey in a group.
 
+7. The ? quantifier matches zero or one occurrence of the preceding character or group. For example, 
+the regular expression /colou?r/ matches both color and colour, because the u is optional.
+
+8. Replace the first literal space with the \s* expression. The \s character class matches whitespace,
+such as spaces, tabs, and new lines. The * quantifier means "match the previous character 0 or more times".
+
+Replace the second literal space with \s+. The + quantifier means "match the previous character at least one time".
+
+9. One last thing with this expression. You don't actually need the match value from your capture group, so you
+can turn it into a non-capturing group. This will allow you to group the characters together without preserving
+the result. To create a non-capturing group in a regular expression, you can add ?: after the opening parenthesis
+of a group. For instance, (?:a|b) will match either a or b, but it will not capture the result. Update your regular
+ expression to use a non-capturing group.
+
 */
 
 const helpRegex = /please help|assist me/i;
-const dollarRegex = /[0-9]+ (hundred|thousand|million|billion) dollars/i;
+const dollarRegex = /[0-9]+\s*(?:hundred|thousand|million|billion)?\s+dollars/i;
+const freeRegex = /(?:^|\s)fr[e3][e3] m[o0]n[e3]y(?:$|\s)/i;
+const stockRegex = /(?:^|\s)[s5][t7][o0][c{[(]k [a@4]l[e3]r[t7](?:$|\s)/i;
+const dearRegex = /(?:^|\s)d[e3][a@4]r fr[i1|][e3]nd(?:$|\s)/i;
 
-const denyList = [helpRegex, dollarRegex];
+const denyList = [helpRegex, dollarRegex, freeRegex, stockRegex, dearRegex];
 
 
 // const isSpam = (msg) => false;  implicity return false
 
 const isSpam = (msg) => denyList.some((regex) => regex.test(msg));
 
-checkMessageButton.addEventListener('click', () => {
+checkMessageButton.addEventListener('click', showOutput)
+
+function showOutput() {
     if (!messageInput.value.trim()) {
         alert("Please enter a message.");
     }
     result.textContent = isSpam(messageInput.value) ? "Oh no! This looks like a spam message." : "This message does not seem to contain any spam.";
     messageInput.value = '';
+};
+
+
+// shift + enter to showOutput
+messageInput.addEventListener('keydown', (e) => {
+  if (e.key === 'Enter' && e.ctrlKey) {
+    e.preventDefault();  // Prevent newline
+    showOutput();        // Run on Shift + Enter
+  } else if (e.key === 'Enter') {
+    // Allow regular Enter to insert a line break (do nothing)
+    // So don't call preventDefault()
+  }
 });
+
+
